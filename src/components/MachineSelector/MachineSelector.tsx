2a0e41dae40db92d6f3a2189986c3e4a84e0e6ae
Rumesh Wickramasinghe <rumesh.wickramasinghe@gmail.com>
2020-03-17 14:48:43 +0530

    feat(machine-selector): change machine selector to material omponent

--- src/components/MachineSelector/MachineSelector.tsx
+++ src/components/MachineSelector/MachineSelector.tsx
@@ -1,6 +1,10 @@
-import React, { useContext, useRef, FC } from 'react';
+import React, { useContext, FC, useEffect } from 'react';
 import './MachineSelector.css';
 import { AppContext, AppContextType } from '../../context/AppContextManager';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 /**
  * This is the machine selector component.
@@ -10,41 +14,60 @@ import { AppContext, AppContextType } from '../../context/AppContextManager';
 const MachineSelector: FC = () => {
   const appContext = useContext<AppContextType>(AppContext);
 
-  const machineSelector = useRef<HTMLSelectElement>(null);
+  const inputLabel = React.useRef<HTMLLabelElement>(null);
+  const [labelWidth, setLabelWidth] = React.useState(0);
+  const [selectItemList, setSelectItemList] = React.useState<
+    JSX.Element[] | null
+  >([]);
 
-  const setSelectedMachine = () => {
-    if (machineSelector.current) {
-      const selectedMachineId = machineSelector.current.value;
-      const selectedMachine = appContext.assets.find(
-        asset => asset.id.toString() === selectedMachineId
-      );
-      appContext.setSelectedMachine(selectedMachine);
+  const updateContext = (machineId: number) => {
+    const selectedMachine = appContext.assets.find(
+      asset => asset.id === machineId
+    );
+    appContext.setSelectedMachine(selectedMachine);
+  };
+
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    if (event.target.value) {
+      updateContext(event.target.value as number);
     }
   };
 
+  useEffect(() => {
+    setLabelWidth(inputLabel.current!.offsetWidth);
+  }, []);
+
+  useEffect(() => {
+    const itemList = appContext.assets
+      ? appContext.assets.map(asset => {
+          return (
+            <MenuItem key={asset.id} value={asset.id}>
+              {asset.name}
+            </MenuItem>
+          );
+        })
+      : null;
+    setSelectItemList(itemList);
+  }, [appContext.assets]);
+
   return (
     <div className="MachineSelector">
-      <div>
-        <label htmlFor="machineSelector">Select Machine</label>
-        <div className="machineSelectWrapper">
-          <select
-            id="machineSelector"
-            className="machineSelectBox"
-            ref={machineSelector}
-            onChange={setSelectedMachine}
-          >
-            {appContext.assets
-              ? appContext.assets.map(asset => {
-                  return (
-                    <option key={asset.id} value={asset.id}>
-                      {asset.name}
-                    </option>
-                  );
-                })
-              : null}
-          </select>
-        </div>
-      </div>
+      <FormControl variant="outlined">
+        <InputLabel ref={inputLabel} id="machineSelector-label">
+          Select Machine
+        </InputLabel>
+        <Select
+          labelId="machineSelector-label"
+          id="machineSelector"
+          value={
+            appContext.selectedMachine ? appContext.selectedMachine.id : ''
+          }
+          onChange={handleChange}
+          labelWidth={labelWidth}
+        >
+          {selectItemList}
+        </Select>
+      </FormControl>
     </div>
   );
 };
