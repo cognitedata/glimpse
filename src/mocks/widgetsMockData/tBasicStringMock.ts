ed535788aacd5635ffc781b6d1e20833035fac90
Asanka <amem.vck@gmail.com>
2020-03-19 18:55:42 +0530

    feat(ts-basic-string): component added

---
+++ src/mocks/widgetsMockData/tBasicStringMock.ts
@@ -0,0 +1,14 @@
+export const TSBasicStringMockProps = [
+  {
+    name: 'Machine State 1',
+    value: 'MANUAL MODE 1',
+    elapsedTime: '00.10.42',
+    isElapsedTimeEnabled: true,
+  },
+  {
+    name: 'Machine State 2',
+    value: 'MANUAL MODE 2',
+    elapsedTime: '00.10.43',
+    isElapsedTimeEnabled: false,
+  },
+];
