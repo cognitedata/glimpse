85221ff347272dda2d299aa074ec5fce83fc8495
Asanka <amem.vck@gmail.com>
2020-03-20 14:47:48 +0530

    feat(ts-basic-numeric): test case failed issue fixed

--- src/components/widgets/TSBasicString/TSBasicString.test.tsx
+++ src/components/widgets/TSBasicString/TSBasicString.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { TSBasicStringMockProps } from 'mocks/widgetsMockData/tSBasicStringMock';
+import { TSBasicStringMockProps } from 'mocks/widgetsMockData/tsBasicMock';
 import TSBasicString from './TSBasicString';
 
 describe('TSBasicString', () => {
