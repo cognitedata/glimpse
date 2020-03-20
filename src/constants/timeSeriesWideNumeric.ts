7ce2800f39d38fb56a7a5d905ea7852668c07929
Rumesh Wickramasinghe <rumesh.wickramasinghe@gmail.com>
2020-03-20 10:56:25 +0530

    feat(time-series-wide-numeric): resolved pr changes

---
+++ src/constants/timeSeriesWideNumeric.ts
@@ -0,0 +1,4 @@
+export const CHART_COLOR = '#6a7095';
+export const CHART_MARGINS = { top: 20, right: 30, left: 0, bottom: 0 };
+export const CHART_STROKE_DASH_ARRAY = '3 3';
+export const CHART_AREA_FILL_OPACITY = 1;
