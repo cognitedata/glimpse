a2531e078fea08046535d33e4ad412ae3c7c529d
Asanka <amem.vck@gmail.com>
2020-03-31 12:45:22 +0530

    test: tests added for dataPointsFetcher saga

--- src/store/sagas/widget.ts
+++ src/store/sagas/widget.ts
@@ -5,13 +5,13 @@ import * as actionTypes from '../actions/actionTypes';
 
 import pollUpdateEventInfo from './widgetSagas/eventsFetcher';
 import pollUpdateDataLatestPoint from './widgetSagas/latestDataPointFetcher';
-import { pollUpdateTsDpsInfo } from './widgetSagas/dataPointsFetcher';
+import { pollUpdateTsDps } from './widgetSagas/dataPointsFetcher';
 
 /* Watcher function for TSDps update polling */
 export function* pollUpdateTsDpsWatcher() {
   while (true) {
     const action = yield take(actionTypes.START_UPDATE_TS_DPS);
-    yield fork(pollUpdateTsDpsInfo, action);
+    yield fork(pollUpdateTsDps, action);
   }
 }
 
