4554a839124e35bd770bab2d98c0ae949812ffb8
Asanka <amem.vck@gmail.com>
2020-04-08 11:08:23 +0530

    fix: pr issue fixes

--- src/components/widgets/showFields/ShowFields.test.tsx
+++ src/components/widgets/showFields/ShowFields.test.tsx
@@ -2,13 +2,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { showFieldsMockProps } from 'mocks/widgetsMockData/showFieldsMock';
-import ShowFieldsFour from './ShowFields';
+import ShowFields from './ShowFields';
 
 describe('ShowField', () => {
   it('should render fields and valuescorrectly', () => {
-    const { getByText } = render(
-      <ShowFieldsFour fields={showFieldsMockProps} />
-    );
+    const { getByText } = render(<ShowFields fields={showFieldsMockProps} />);
     for (let i = 0; i < 4; i++) {
       expect(getByText(showFieldsMockProps[i].label)).toBeInTheDocument();
       expect(getByText(showFieldsMockProps[i].value)).toBeInTheDocument();
