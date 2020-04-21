4857d7decb61d5a61db50a1d3d62d8070771e0a2
Asanka <amem.vck@gmail.com>
2020-04-22 02:33:32 +0530

    feat: widgetRemove/reposition added for localstorage

--- src/components/widgetCRUD/widgetCruds.ts
+++ src/components/widgetCRUD/widgetCruds.ts
@@ -4,15 +4,40 @@ import { WidgetConfig } from 'components/grid/interfaces';
 export const saveWidgetConfig = async (
   userId: string,
   assetId: string,
-  grid: any
+  widgetConf: any
 ) => {
   const gridConf = JSON.parse(localStorage.getItem(userId) || '{}');
   gridConf[assetId] = gridConf[assetId]
-    ? gridConf[assetId].concat(grid)
-    : [grid];
+    ? gridConf[assetId].concat(widgetConf)
+    : [widgetConf];
   localStorage.setItem(userId, JSON.stringify(gridConf));
   return true;
 };
+
+export const saveWidgetConfigs = async (
+  userId: string,
+  assetId: string,
+  widgetConfs: any
+) => {
+  const gridConf = JSON.parse(localStorage.getItem(userId) || '{}');
+  gridConf[assetId] = widgetConfs;
+  localStorage.setItem(userId, JSON.stringify(gridConf));
+  return true;
+};
+
+export const deleteWidgetConfig = async (
+  userId: string,
+  assetId: string,
+  widgetId: string
+) => {
+  const gridConf = JSON.parse(localStorage.getItem(userId) || '{}');
+  const widgetConfigs: WidgetConfig[] = gridConf[assetId] || [];
+  const filteredWidgetConfs = widgetConfigs.filter(conf => conf.i !== widgetId);
+  gridConf[assetId] = filteredWidgetConfs;
+  localStorage.setItem(userId, JSON.stringify(gridConf));
+  return true;
+};
+
 export const getWidgetConfigByUserId = async (
   userid: any
 ): Promise<WidgetConfigs> => {
