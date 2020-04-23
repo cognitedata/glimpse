99754e50fc3c53f25083cfa5fcc268e689f2f47d
Asanka <amem.vck@gmail.com>
2020-04-23 13:05:21 +0530

    fix: refactoring done

---
+++ src/components/widgetCRUD/repo/widgstConfFSRepo.ts
@@ -0,0 +1,69 @@
+// Copyright 2020 Cognite AS
+import { WidgetConfig } from 'components/grid/interfaces';
+import { widgetConfFSDoc } from '../../../firebase';
+
+export const save = async (
+  userId: string,
+  assetId: string,
+  widgetConf: any
+) => {
+  const gridConfDoc = await widgetConfFSDoc(userId).get();
+
+  let gridConf: any = {};
+  if (gridConfDoc.exists) {
+    gridConf = gridConfDoc.data();
+    gridConf[assetId] = gridConf[assetId]
+      ? gridConf[assetId].concat(widgetConf)
+      : [widgetConf];
+    await widgetConfFSDoc(userId).update(gridConf);
+  }
+
+  gridConf[assetId] = [widgetConf];
+  await widgetConfFSDoc(userId).set(gridConf);
+};
+
+export const update = async (
+  userId: string,
+  assetId: string,
+  widgetConfs: any
+) => {
+  const gridConfDoc = await widgetConfFSDoc(userId).get();
+  let widgetConfObj: WidgetConfigs = {};
+  if (gridConfDoc.exists) {
+    widgetConfObj = gridConfDoc.data();
+    widgetConfObj[assetId] = widgetConfs;
+    await widgetConfFSDoc(userId).update(widgetConfObj);
+  }
+  widgetConfObj[assetId] = widgetConfs;
+  await widgetConfFSDoc(userId).set(widgetConfObj);
+};
+
+export const deleteOne = async (
+  userId: string,
+  assetId: string,
+  widgetId: string
+) => {
+  const gridConfDoc = await widgetConfFSDoc(userId).get();
+
+  if (gridConfDoc.exists) {
+    const widgetConfObj = gridConfDoc.data();
+    const widgetConfigs: WidgetConfig[] = widgetConfObj[assetId] || [];
+    const filteredWidgetConfs = widgetConfigs.filter(
+      conf => conf.i !== widgetId
+    );
+    widgetConfObj[assetId] = filteredWidgetConfs;
+    await widgetConfFSDoc(userId).update(widgetConfObj);
+  }
+};
+
+export const getByUser = async (userId: any): Promise<WidgetConfigs> => {
+  const gridConfDoc = await widgetConfFSDoc(userId).get();
+  if (gridConfDoc.exists) {
+    return gridConfDoc.data();
+  }
+  return {};
+};
+
+type WidgetConfigs = {
+  [key: string]: WidgetConfig[];
+};
---
+++ src/components/widgetCRUD/services/widgetConfService.ts
@@ -0,0 +1,72 @@
+// Copyright 2020 Cognite AS
+import { Layout } from 'react-grid-layout';
+import findIndex from 'lodash/findIndex';
+import { WidgetConfig } from 'components/grid/interfaces';
+import { getByUser, update, deleteOne, save } from '../repo/widgstConfFSRepo';
+
+export const getWidgetConfigs = async (userId: string, onError: Function) => {
+  try {
+    return await getByUser(userId);
+  } catch (error) {
+    onError('Unable to fetch saved widget configerations data!');
+    return {};
+  }
+};
+
+export const updateLayout = async (
+  userId: string,
+  assetId: string,
+  changedlayOuts: Layout[],
+  widgetConfigs: WidgetConfig[],
+  onError: Function
+) => {
+  const newWidgetConfigs = [...widgetConfigs];
+  changedlayOuts.forEach(layout => {
+    const { i, x, y } = layout;
+    const index = findIndex(widgetConfigs, { i });
+    if (index !== -1) {
+      const newWidgetConfig: WidgetConfig = {
+        ...widgetConfigs[index],
+        cordinates: [x, y],
+      };
+      newWidgetConfigs[index] = newWidgetConfig;
+    }
+  });
+  try {
+    await update(userId, assetId, newWidgetConfigs);
+    return true;
+  } catch (error) {
+    onError('layOuts Save Failed');
+    return false;
+  }
+};
+
+export const saveWidget = async (
+  userId: string,
+  assetId: string,
+  newWidgetConf: WidgetConfig,
+  onError: Function
+) => {
+  try {
+    await save(userId, assetId, newWidgetConf);
+    return true;
+  } catch (error) {
+    onError('Widget save failed');
+    return false;
+  }
+};
+
+export const deleteWidget = async (
+  userId: string,
+  assetId: string,
+  widgetId: string,
+  onError: Function
+) => {
+  try {
+    await deleteOne(userId, assetId, widgetId);
+    return true;
+  } catch (error) {
+    onError('Unable to delete the widget');
+    return false;
+  }
+};
