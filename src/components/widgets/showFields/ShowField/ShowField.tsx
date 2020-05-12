f1c907a6768e582c5eab3c2c8cd7d3c36d74f11f
Asanka <amem.vck@gmail.com>
2020-05-12 11:20:34 +0530

    refactor: events data view enhancements
    fix: pr issue fixes

--- src/components/widgets/showFields/ShowField/ShowField.tsx
+++ src/components/widgets/showFields/ShowField/ShowField.tsx
@@ -1,6 +1,8 @@
 // Copyright 2020 Cognite AS
 import React, { FC } from 'react';
 import './ShowField.css';
+import moment from 'moment';
+import { isFloat, roundValue } from 'utils/utils';
 import { ShowFieldProps } from './interfaces';
 
 /**
@@ -8,13 +10,36 @@ import { ShowFieldProps } from './interfaces';
  * @param props ShowFieldProps
  */
 const ShowField: FC<ShowFieldProps> = (props: ShowFieldProps) => {
-  const { label, value } = props;
+  const { label, value, path } = props;
+  /**
+   * format returning object based on the input object type.
+   * @param valueProp any
+   */
+  const formatValueByType = (valueProp: any) => {
+    if (!valueProp) {
+      return ' - ';
+    }
+    // if substring 'time or Date' contains in the path it should be a time
+    if (
+      path &&
+      new RegExp(/(time|date)/gi).test(path) &&
+      new Date(valueProp).getTime() > 0
+    ) {
+      return moment(valueProp).format('YYYY-MM-DD HH:mm:ss');
+    }
+    if (typeof valueProp === 'object') {
+      return JSON.stringify(valueProp);
+    }
+    if (isFloat(valueProp)) {
+      return roundValue(valueProp);
+    }
+    return valueProp;
+  };
+
   return (
     <div className="show-field">
       <span className="field">{label}</span>
-      <span className="value">
-        {typeof value === 'object' ? JSON.stringify(value) : value}
-      </span>
+      <span className="value">{formatValueByType(value)}</span>
     </div>
   );
 };
